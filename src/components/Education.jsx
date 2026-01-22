a2a2ec3babb596f0b165bd408640981ba5f95d0b
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-01-22 16:20:32 -0600

    feat: allow user to input degree information
    Form able to process user degree information for
    each school

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -14,7 +14,27 @@ function EducationSectionForm() {
                     <input type="text" name="location-name" id="location-name" />
                 </div>
             </fieldset>
-            
+            <fieldset>
+                <legend>Degree Information</legend>
+                <div className="input-field">
+                    <label htmlFor="degree-name">Degree</label>
+                    <input type="text" name="degree-name" id="degree-name" />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="study-name">Field of study</label>
+                    <input type="text" name="study-name" id="study-name"/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="start-date">Start date</label>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="end-date">End date</label>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="grade-info">GPA</label>
+                    <input type="text" name="grade-info" id="grade-info"/>
+                </div>
+            </fieldset>
         </form>
     )
 }
