2376fc4100c50f9245bd25c41b2a63bea9a0d8fa
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-01-27 19:46:46 -0600

    fix: closing gen form no longer resets form inputs
    User input no longer reset after user close or open
    of general information form.

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -4,10 +4,18 @@ import Icon from '@mdi/react';
 import { mdiAccount } from '@mdi/js';
 import { mdiChevronDown,mdiChevronUp } from '@mdi/js';
 
-
 function GeneralSection() {
     const [showLink, setShowLink] = useState(false);
     const [showSection, setShowSection] = useState(false);
+    const [generalInput, setGeneralInput] = useState({
+        name: "",
+        email: "",
+        number: "",
+        linkedIn: "",
+        github: "",
+        id: crypto.randomUUID()
+    })
+    
     const onBtnClick = (currentView) => () => {
         const newView = currentView ? false : true;
         setShowLink(newView);
@@ -18,7 +26,6 @@ function GeneralSection() {
         setShowSection(newView);
     }
 
-    
     return (
         <section className="general-section">
             <div className="header-section">
@@ -33,51 +40,85 @@ function GeneralSection() {
                         <Icon path={mdiChevronUp} className="header-icon" />
                     </button>}
             </div>
-            {showSection && <GeneralSectionForm/>}
+            {showSection && <GeneralSectionForm userInput = {generalInput} callBack={setGeneralInput}/>}
             {showSection && <div className="additional-link-section">
                 <button 
                   type="button"
                   onClick={onBtnClick(showLink)}>
                     Additional Links
                 </button>
-                {showLink && <GeneralSectionFormLinks/>}
+                {showLink && <GeneralSectionFormLinks userInput = {generalInput} callBack={setGeneralInput} />}
             </div>}
         </section>
     )
 }
 
-function GeneralSectionForm() {
+function GeneralSectionForm({userInput, callBack}) {
     return (
         <form className="general-info">
             <div className="input-field">
                 <label htmlFor="general-name">Full Name</label>
-                <input type="text" name="general-name" id="general-name" />
+                <input type="text" name="general-name" id="general-name" 
+                value={userInput.name} onChange={(event) => {
+                    callBack({...userInput, name: event.target.value})
+                }}/>
             </div>
             <div className="input-field">
                 <label htmlFor="general-email">Email</label>
-                <input type="email" name="general-email" id="general-email" />
+                <input type="email" name="general-email" id="general-email" 
+                 value={userInput.email} onChange={(event) => {
+                    callBack({...userInput, email: event.target.value})
+                }}/>
             </div>
             <div className="input-field">
                 <label htmlFor="general-phone">Phone Number</label>
-                <input type="email" name="general-phone" id="general-phone" />
+                <input type="tel" name="general-phone" id="general-phone" 
+                value={userInput.number} onChange={(event) => {
+                    callBack({...userInput, number: event.target.value})
+                }}/>
             </div>
         </form>
     )
 }
 
-function GeneralSectionFormLinks() {
+function GeneralSectionFormLinks({userInput, callBack}) {
     const supportedLinks = ["LinkedIn", "GitHub"];
     
     return (
         <div className="input-links">
             <form className="general-info-links">
-            {supportedLinks.map((link)=> {
-                return (
-                <div className="input-box">
-                    <label htmlFor={"general-link-" + link.toLocaleLowerCase()}>{link}</label>
-                    <input type="url" name={"general-link-" + link.toLocaleLowerCase()} id={"general-link-" + link.toLocaleLowerCase()}/>
-                </div> 
+            {supportedLinks.map((link, index)=> {
+                if(link == "LinkedIn")
+                {
+                    return (
+                    <div className="input-box" key={userInput.id + index}>
+                        <label htmlFor={"general-link-" + link.toLocaleLowerCase()}>{link}</label>
+                        <input type="url" name={"general-link-" + link.toLocaleLowerCase()} id={"general-link-" + link.toLocaleLowerCase()}
+                        value={userInput.linkedIn} onChange={(event) => {
+                            callBack({...userInput, linkedIn: event.target.value})
+                        }}/>
+                    </div> 
+                    )
+                }
+                else if(link == "GitHub") {
+                     return (
+                    <div className="input-box" key={userInput.id + index}>
+                        <label htmlFor={"general-link-" + link.toLocaleLowerCase()}>{link}</label>
+                        <input type="url" name={"general-link-" + link.toLocaleLowerCase()} id={"general-link-" + link.toLocaleLowerCase()}
+                        value={userInput.github} onChange={(event) => {
+                            callBack({...userInput, github: event.target.value})
+                        }}/>
+                    </div> 
+                    )
+                }
+                else {
+                    return (
+                    <div className="input-box" key={userInput.id + index}>
+                        <label htmlFor={"general-link-" + link.toLocaleLowerCase()}>{link}</label>
+                        <input type="url" name={"general-link-" + link.toLocaleLowerCase()} id={"general-link-" + link.toLocaleLowerCase()}/>
+                    </div> 
                 )
+                }
             })}
             </form>
         </div>
