4ad6272e49aef1240f8e80ea980cbb180eb4e60a
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2026-03-07 15:44:42 -0600

    fix: prev. screen no longer breaks on small screen

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,9 +2,16 @@ import { Resume } from "./resume";
 import Icon from '@mdi/react';
 import { mdiClose } from '@mdi/js';
 
+
 export function PreviewWindow({generalInput, educationInput, expInput, summaryInput, skillsInput, setShowPreview}) {
+    
+    const resumeWidth = document.querySelector(".resume").getBoundingClientRect().width
+    console.log(resumeWidth);
+
     return (
-        <dialog open className="modal">
+        <dialog open className="modal" style={ {
+            width: `${resumeWidth}px`
+        }}>
             <Resume generalInput={generalInput} educationInput={educationInput} expInput={expInput} summaryInput={summaryInput} skillsInput={skillsInput}/>
             <button className="modal-exit" onClick={() => {
                 setShowPreview(false)
